a1a78c396f9359d436c66cc853242bbf2df5f380
Deval Mavani <deval@Devals-Mac-mini.local>
2026-04-13 18:18:38 +0530

    UI Changes

--- app.py
+++ app.py
@@ -3,23 +3,34 @@ from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
+# History of conversations
+history = []
+
 @app.route('/')
 def home():
-    return render_template('base.html', response='')
+    return render_template('base.html', response='', history=history)
 
-@app.route('/submit', methods= ['POST'])
+@app.route('/submit', methods=['POST'])
 def submit():
     user_input = request.form.get('message')
-    print("Entered Text: ",user_input)
-
-    response = ollama.generate(model='llama2', prompt=f'create caption on this {user_input}', stream=True)
+    response = ollama.generate(
+        model='llama2',
+        prompt=f'create caption on this {user_input}',
+        stream=True
+    )
 
     output = ''
     for i in response:
         output += i['response']
-    print('Answer: ',output)
-    
-    return render_template('base.html', response=output)
+
+    # Save history
+    history.append({
+        'user': user_input,
+        'bot': output
+    })
+
+    return render_template('base.html', response=output, history=history)
+
 
 if __name__ == "__main__" :
     app.run(debug=True)
